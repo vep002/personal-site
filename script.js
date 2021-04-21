a579a1825045face76ad3ad6921acf4764c90891
Victoria <vep002@gmail.com>
2021-04-21 10:19:28 -0500

    fixed js issue

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fiber2 = document.getElementById('fiber');
+/*const fiber2 = document.getElementById('fiber');
 const other2 = document.getElementById('other');
 const code2 = document.getElementById('code');
 
@@ -10,4 +10,12 @@ other2.addEventListener('click', function() {
 });
 code2.addEventListener('click', function() {
     alert('Watch this space for future projects!')
-});
+});*/
+
+
+fiber.addEventListener('click', function() {
+    alert('Watch this space for future fiber arts projects!')})
+other.addEventListener('click', function() {
+    alert('Watch this space for future mixed media projects!')})
+code.addEventListener('click', function() {
+    alert('Watch this space for future code projects!')})
